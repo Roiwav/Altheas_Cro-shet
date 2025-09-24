9c05b70cb28d05acb3fa078aa2c2d5246fdd7c66
Roiwav <jrtp03085@gmail.com>
2025-09-25 04:57:46 +0800

    test server

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Your frontend URL
+    origin: ["http://localhost:5173", "https://altheas-crochet-project.vercel.app/"],// Your frontend URL
     methods: ["GET", "POST"]
   }
 });
